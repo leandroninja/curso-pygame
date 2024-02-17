1f77ba51b1586d810942776c214f4f85f0902f97
leandroninja <riey21@hotmail.com>
2024-02-17 13:45:28 -0300

    adicionado movimento do objeto segurando e precionando uma tecla

--- janela.py
+++ janela.py
@@ -10,7 +10,7 @@ largura = 640
 altura = 480
 # varialvel x,y controla movimento do retangulo
 x = largura/2
-y = 0
+y = altura/2
 
 
 tela = pygame.display.set_mode((largura, altura))
@@ -18,16 +18,37 @@ pygame.display.set_caption('janela do jogo')
 relogio = pygame.time.Clock()
 
 while True:
-    relogio.tick(45)
+    relogio.tick(30)
     tela.fill((0,0,0))
     for event in pygame.event.get():
         if event.type == QUIT:
            pygame.QUIT()
            exit()
+        
+        '''
+
+        if event.type == KEYDOWN:
+            if event.key == K_LEFT: 
+                x = x - 20  
+            if event.key == K_RIGHT:
+                x = x + 20
+            if event.key == K_UP:
+                y = y - 20 
+            if event.key == K_DOWN:
+                y = y + 20'''
+            
+
+    if pygame.key.get_pressed()[K_LEFT]:
+       x = x - 20
+    if pygame.key.get_pressed()[K_RIGHT]:
+       x = x + 20  
+    if pygame.key.get_pressed()[K_UP]:
+       y = y - 20 
+    if pygame.key.get_pressed()[K_DOWN]:
+       y = y + 20      
+
     pygame.draw.rect(tela, (255,0,0), (x, y,40,50)) 
-    if y >= altura:
-        y = 0
-    y = y + 1
+    
 
 
    # pygame.draw.circle(tela,(0,0,255), (300,260), 40)   
